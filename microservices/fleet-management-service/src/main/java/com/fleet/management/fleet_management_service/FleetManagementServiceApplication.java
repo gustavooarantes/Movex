a30ddd9bc28c5a6e3d03a43f177d5f4bd2224a1b
Gustavo <profissional.gustavoa@gmail.com>
2025-07-27 18:10:50 -0300

    feat: Add fleet-management-service module using Spring Initializr

---
+++ microservices/fleet-management-service/src/main/java/com/fleet/management/fleet_management_service/FleetManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fleet.management.fleet_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetManagementServiceApplication.class, args);
+	}
+
+}
---
+++ microservices/fleet-management-service/src/test/java/com/fleet/management/fleet_management_service/FleetManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleet.management.fleet_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
